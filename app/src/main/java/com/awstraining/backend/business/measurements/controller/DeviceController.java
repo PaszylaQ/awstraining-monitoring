00bad852435567911f2491ef41beadd6d2f56969
PaszylaQ <maciekpaszylka@gmail.com>
2024-09-20 11:13:16 +0200

    PaszylaQ; quick fix

--- app/src/main/java/com/awstraining/backend/business/measurements/controller/DeviceController.java
+++ app/src/main/java/com/awstraining/backend/business/measurements/controller/DeviceController.java
@@ -42,7 +42,7 @@ class DeviceController implements DeviceIdApi {
                 .stream()
                 .map(this::toMeasurement)
                 .toList();
-        LOGGER.info("Size of measurements '{}'", measurements.length());        
+        LOGGER.info("Size of measurements '{}'", measurements.size());        
         final Measurements measurementsResult = new Measurements();
         measurementsResult.measurements(measurements);
         return ResponseEntity.ok(measurementsResult);
